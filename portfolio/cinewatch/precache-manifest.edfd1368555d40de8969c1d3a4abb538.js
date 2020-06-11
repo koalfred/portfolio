c63437189825d3dcd6c0f9b7921200738438809d
Alfred Ko <aktc411@gmail.com>
2020-06-11 11:31:55 -0700

    update 11062020

---
+++ portfolio/cinewatch/precache-manifest.edfd1368555d40de8969c1d3a4abb538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65f94d0b68e4e384c18ff3f90479099",
+    "url": "/cinewatch/index.html"
+  },
+  {
+    "revision": "e6f524c5cf9c04efaa65",
+    "url": "/cinewatch/static/css/main.096e85ad.chunk.css"
+  },
+  {
+    "revision": "16bc9cfb378ab4ebd565",
+    "url": "/cinewatch/static/js/2.0de668ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cinewatch/static/js/2.0de668ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f524c5cf9c04efaa65",
+    "url": "/cinewatch/static/js/main.28f3d902.chunk.js"
+  },
+  {
+    "revision": "e6fbcc200c6185a42626",
+    "url": "/cinewatch/static/js/runtime-main.18eaf204.js"
+  }
+]);
