39253325ae0270d669942cf030a79d445b079b02
Alfred Ko <aktc411@gmail.com>
2022-07-10 16:33:18 -0600

    Contact form update

--- contact-form.php
+++ contact-form.php
@@ -9,29 +9,31 @@
   </head>
   <body>
     <?php include("includes/nav.php"); ?>
-    <div class="wrapper">
-      <?php
-        if (isset($_POST['submit']) && isset($_POST['fname']) && isset($_POST['email'])) {
-            $first_name = $_POST['fname'];
-            $last_name = $_POST['lname'];
-            $email = $_POST['email'];
-            $message = $_POST['message'];
+    <div class="main_body">
+      <div class="wrapper">
+        <?php
+          if (isset($_POST['submit']) && isset($_POST['fname']) && isset($_POST['email'])) {
+              $first_name = $_POST['fname'];
+              $last_name = $_POST['lname'];
+              $email = $_POST['email'];
+              $message = $_POST['message'];
 
-            $to = "ako@alfred-frontend.ca";
-            $subject = "From: ".$email;
-            $txt = "You have received an email from ".$first_name.".\n\n".$message;
-            $headers = "From: ".$email;
+              $to = "ako@alfred-frontend.ca";
+              $subject = "From: ".$email;
+              $txt = "You have received an email from ".$first_name.".\n\n".$message;
+              $headers = "From: ".$email;
 
-            if(mail($to, $subject, $txt, $headers)) {
-                echo "<h2>Message sent successfully!</h2>
-                      <p>Thank you very much for your message. I will be in contact with you shortly.</p>
-                      <p>I look forward to working with you!</p>";
-            } else {
-                echo "<h1>Something went wrong.</h1>";
-            };
-        }
-      ?>
-    </div> 
+              if(mail($to, $subject, $txt, $headers)) {
+                  echo "<h2>Message sent successfully!</h2>
+                        <p>Thank you very much for your message. I will be in contact with you shortly.</p>
+                        <p>I look forward to working with you!</p>";
+              } else {
+                  echo "<h1>Something went wrong.</h1>";
+              };
+          }
+        ?>
+      </div> 
+    </div>
     <?php include("includes/footer.php"); ?>
     <script src="scripts/scripts.js"></script>
   </body>
